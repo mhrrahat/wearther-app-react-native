071989f7a4ad18d5dbbbd4971f59e533f8b5346f
mhrrahat <mhrahat@mrftechnologies.com>
2021-08-18 03:24:53 -0800

    last commit

--- App.js
+++ App.js
@@ -1,12 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import Weather from "./components/Weather";
+import SearchBar from "./components/SearchBar";
+
+const API_KEY = "9848012d5c5911624a78de34a61d572f";
 
 export default function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [loaded, setLoaded] = useState(true);
+
+  async function fetchWeatherData(cityName) {
+    setLoaded(false);
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`;
+    try {
+      const response = await fetch(API);
+      if (response.status == 200) {
+        const data = await response.json();
+        setWeatherData(data);
+      } else {
+        setWeatherData(null);
+      }
+      setLoaded(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchWeatherData("Mymensingh");
+  }, []);
+
+  if (!loaded) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator color="gray" size={36} />
+      </View>
+    );
+  } else if (weatherData === null) {
+    return (
+      <View style={styles.container}>
+        <SearchBar fetchWeatherData={fetchWeatherData} />
+        <Text style={styles.primaryText}>
+          City Not Found! Try Different City
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData} />
     </View>
   );
 }
@@ -14,8 +58,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  primaryText: {
+    margin: 20,
+    fontSize: 28,
   },
 });
